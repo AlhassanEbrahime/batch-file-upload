0844751e8f16cee979a39ac2253f531bdd66288a
AlhassanEbrahime <hasan16121973@gmail.com>
2025-10-18 09:20:13 +0300

    excelupload

---
+++ excelfileupload/src/main/java/file/upload/excelfileupload/ExcelfileuploadApplication.java
@@ -0,0 +1,13 @@
+package file.upload.excelfileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelfileuploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelfileuploadApplication.class, args);
+    }
+
+}
---
+++ excelfileupload/src/test/java/file/upload/excelfileupload/ExcelfileuploadApplicationTests.java
@@ -0,0 +1,13 @@
+package file.upload.excelfileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelfileuploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
